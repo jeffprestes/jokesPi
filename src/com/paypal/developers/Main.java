34ea6f91556a0ed53fa7521e0677c4fed015c9a0
Jeff Prestes <jeffprestes@gmail.com>
2014-12-01 11:27:15 -0200

    Adjust Main class name

--- src/com/paypal/developers/Main.java
+++ src/com/paypal/developers/Main.java
@@ -14,7 +14,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         // TODO code application logic here
     }
     
