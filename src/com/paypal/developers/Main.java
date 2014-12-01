121e81d589072bfb196f472f58190fe8d6f13d0f
Jeff Prestes <jeffprestes@gmail.com>
2014-12-01 14:29:52 -0200

    Now the machine is already speaking

--- src/com/paypal/developers/Main.java
+++ src/com/paypal/developers/Main.java
@@ -5,6 +5,12 @@
  */
 package com.paypal.developers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jprestes
@@ -14,8 +20,40 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void Main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {
+        try {
+            
+            String system = System.getProperty("os.name");
+            System.out.println(system);
+            
+            Process proc = null;
+            //TODO: Figure out how use eSpeak on Windows
+            if (system.contains("windows")) {
+                
+            
+            } else if (system.contains("Mac"))  {
+                proc = Runtime.getRuntime().exec("/usr/local/Cellar/espeak/1.48.04/bin/espeak -m -vpt -s140 'muito&nbsp;obrigado'");
+            
+            } else  {
+                proc = Runtime.getRuntime().exec("/usr/bin/espeak -m -vpt -s140 'muito&nbsp;obrigado'");
+            }
+            
+            //proc.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+            System.out.println("Program terminated!");
+
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+       // } catch (InterruptedException ex) {
+        //    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
